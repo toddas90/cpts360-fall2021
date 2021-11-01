ef6a3cce56c6d0780cc3976307c0aa51593d7091
Andrew Todd <andrew123ast@gmail.com>
2021-11-01 11:54:32 -0700

    Added the decFreeInodes code from the book.

--- final/src/mkdir_creat.c
+++ final/src/mkdir_creat.c
@@ -9,6 +9,9 @@
 #include "../include/type.h"
 
 extern char pathname[128];
+extern int dev;
+extern SUPER *sp;
+extern GD = *gp;
 
 int lab_mkdir() {
     // Check for empty global pathname
@@ -36,6 +39,21 @@ int set_bit(char *buf, int bit) {
 }
 
 int dec_free_inodes(int dev) {
+    char buf[BLKSIZE];
+
+    // I want to check for errors in this, but
+    // I'd have to modify KC's get/put_block code
+    // in util.c first.
+
+    get_block(dev, 1, buf); // get block into buf
+    sp = (SUPER *)buf; // cast buf as SUPER
+    sp->s_free_inodes_count -= 1; // decrement inode counter
+    put_block(dev, 1, buf); // write buf back
+
+    get_block(dev, 2, buf); // get gd into buf
+    gp = (GD *)buf; // cast buf as GD
+    gp->bg_free_inodes_count -= 1; // decrement inode count
+    put_block(dev, 2, buf); // write buf back
     return 0;
 }
 
