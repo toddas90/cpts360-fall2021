890ac5d2117cf4d8d9d43c0a853e0003949d177f
Andrew Todd <andrew123ast@gmail.com>
2021-11-09 12:49:45 -0800

    Started writing symlink. Need to sprint to class

--- final/src/symlink.c
+++ final/src/symlink.c
@@ -13,6 +13,54 @@
 #include "../include/colors.h"
 #include "../include/type.h"
 
+extern char pathname[128], extra_arg[128];
+extern int dev;
+extern PROC *running;
+
 int symlink() {
+    if (!strcmp(pathname, "")) {
+        printf(YEL "Source file not found\n" RESET);
+        return -1;
+    }
+
+    if (!strcmp(extra_arg, "")) {
+        printf(YEL "New file not provided\n" RESET);
+        return -1;
+    }
+
+    int old_ino = getino(pathname);
+    if (old_ino <= 0) {
+        printf(YEL "File does not exist\n" RESET);
+        return -1;
+    }
+
+    MINODE *old_mip = iget(dev, old_ino);
+
+    if (getino(extra_arg) != 0) {
+        printf(YEL "File already exists\n" RESET);
+        return -1;
+    }
+
+    char *copy_pathname[128];
+    strcpy(copy_pathname, pathname);
+    strcpy(pathname, extra_arg);
+
+    lab_creat(); // create new file
+
+    int ino = getino(extra_arg); // get new file
+    MINODE *mip = iget(dev, ino);
+
+    mip->INODE.i_mode = 0xA1FF; // set link type
+
+    // NEED TO FINISH
+    
+    // assume length of old_file name <= 60 chars
+        // store old_file name in newfile's INODE.i_block[] area
+        // set file size to length of old_file name
+        // mark new_file's minode dirty
+        // iput new_file
+    // mark new_file's parent minode dirty
+    // iput new_file's parent.
+
     return 0;
 }
