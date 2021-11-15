ffc76d6499ca6e0ff76c5919087e9ffff28464c2
Andrew Todd <andrew123ast@gmail.com>
2021-11-15 10:23:55 -0800

    Removed questions file for level 1 demo. Added open.c

---
+++ final/src/open.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <ext2fs/ext2fs.h>
+#include <sys/stat.h>
+#include <fnctl.h>
+
+#include "../include/open.h"
+#include "../include/util.h"
+#include "../include/colors.h"
+#include "../include/type.h"
+
+int my_open() {
+    
+}
