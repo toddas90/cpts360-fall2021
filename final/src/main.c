63db51cf1f4acac1b437b9ba3dda550de0d1e17e
Andrew Todd <andrew123ast@gmail.com>
2021-11-30 12:15:08 -0800

    Got the disks downloaded for the level 3 demo. Changed the main() code to allow for entering a disk name.

--- final/src/main.c
+++ final/src/main.c
@@ -72,11 +72,10 @@ int init()
 }
 
 // load root INODE and set root pointer to it
-int mount_root()
-{  
-  printf("mounting root...\n");
-  root = iget(dev, 2);
-  return 0;
+int mount_root() { 
+    printf("mounting root...\n");
+    root = iget(dev, 2);
+    return 0;
 }
 
 int quit()
@@ -91,104 +90,109 @@ int quit()
   exit(0);
 }
 
-char *disk = "diskimage";
-int main(int argc, char *argv[ ])
-{
-  int ino;
-  char buf[BLKSIZE];
+char disk[32];
+//char *disk = "diskimage";
+int main(int argc, char *argv[ ]) {
+    int ino;
+    char buf[BLKSIZE];
 
-  printf("checking EXT2 FS ....");
-  if ((fd = open(disk, O_RDWR)) < 0){
-    printf(RED "open %s failed\n" RESET, disk);
-    exit(1);
-  }
-
-  dev = fd;    // global dev same as this fd   
-
-  /********** read super block  ****************/
-  get_block(dev, 1, buf);
-  sp = (SUPER *)buf;
-
-  /* verify it's an ext2 file system ***********/
-  if (sp->s_magic != 0xEF53){
-      printf(RED "magic = %x is not an ext2 filesystem\n" RESET, sp->s_magic);
-      exit(1);
-  }     
-  printf(" OK\n");
-  ninodes = sp->s_inodes_count;
-  nblocks = sp->s_blocks_count;
-
-  get_block(dev, 2, buf); 
-  gp = (GD *)buf;
-
-  bmap = gp->bg_block_bitmap;
-  imap = gp->bg_inode_bitmap;
-  iblk = gp->bg_inode_table;
-  //printf("bmp=%d imap=%d inode_start = %d\n", bmap, imap, iblk);
-
-  init();  
-  mount_root();
-  //printf("root refCount = %d\n", root->refCount);
-
-  //printf("creating P0 as running process\n");
-  running = &proc[0];
-  running->status = READY;
-  running->cwd = iget(dev, 2);
-  //printf("root refCount = %d\n", root->refCount);
-
-  // WRTIE code here to create P1 as a USER process
-
-  printf("\n");
-  my_banner();
-  printf("\nEnter a command or type \"help\"\n");
-
-  
-  while(1){
-    printf(BLD MAG""RESET);
-    fgets(line, 128, stdin);
-    line[strlen(line)-1] = 0;
-
-    if (line[0]==0)
-       continue;
-    pathname[0] = 0;
-
-    sscanf(line, "%s %s %s", cmd, pathname, extra_arg);
-    printf(RESET);
-    //printf("cmd=%s pathname=%s\n", cmd, pathname);
-  
-    if (strcmp(cmd, "ls")==0)
-       ls();
-    else if (strcmp(cmd, "cd")==0)
-       cd();
-    else if (strcmp(cmd, "pwd")==0)
-       pwd(running->cwd);
-    else if (strcmp(cmd, "mkdir")==0)
-        my_mkdir(pathname);
-    else if (strcmp(cmd, "rmdir")==0)
-        my_rmdir();
-    else if (strcmp(cmd, "link")==0)
-        my_link();
-    else if (strcmp(cmd, "unlink")==0)
-        my_unlink();
-    else if (strcmp(cmd, "symlink")==0)
-        my_symlink();
-    else if (strcmp(cmd, "creat")==0)
-        my_creat(pathname);
-    else if (strcmp(cmd, "quit")==0)
-       quit();
-    else if (strcmp(cmd, "clear")==0)
-        clear();
-    else if (strcmp(cmd, "cat")==0)
-        cat(pathname);
-    else if (strcmp(cmd, "cp")==0)
-        cp(pathname, extra_arg);
-    else if (strcmp(cmd, "pfd")==0)
-        print_fd();
-    else if (strcmp(cmd, "help")==0)
-        help();
+    // Get disk from user
+    printf("Enter disk to mount: ");
+    fgets(disk, 32, stdin);
+    disk[strlen(disk)-1] = 0;
     printf("\n");
-    bzero(pathname, 128);
-  }
+
+    printf("checking EXT2 FS ....");
+    if ((fd = open(disk, O_RDWR)) < 0){
+        printf(RED "open %s failed\n" RESET, disk);
+        exit(1);
+    }
+
+    dev = fd;    // global dev same as this fd   
+
+    /********** read super block  ****************/
+    get_block(dev, 1, buf);
+    sp = (SUPER *)buf;
+
+    /* verify it's an ext2 file system ***********/
+    if (sp->s_magic != 0xEF53){
+        printf(RED "magic = %x is not an ext2 filesystem\n" RESET, sp->s_magic);
+        exit(1);
+    }     
+    printf(" OK\n");
+    ninodes = sp->s_inodes_count;
+    nblocks = sp->s_blocks_count;
+
+    get_block(dev, 2, buf); 
+    gp = (GD *)buf;
+
+    bmap = gp->bg_block_bitmap;
+    imap = gp->bg_inode_bitmap;
+    iblk = gp->bg_inode_table;
+    //printf("bmp=%d imap=%d inode_start = %d\n", bmap, imap, iblk);
+
+    init();  
+    mount_root();
+    //printf("root refCount = %d\n", root->refCount);
+
+    //printf("creating P0 as running process\n");
+    running = &proc[0];
+    running->status = READY;
+    running->cwd = iget(dev, 2);
+    //printf("root refCount = %d\n", root->refCount);
+
+    // WRTIE code here to create P1 as a USER process
+
+    printf("\n");
+    my_banner();
+    printf("\nEnter a command or type \"help\"\n");
+ 
+    while(1){
+        printf(BLD MAG""RESET);
+        fgets(line, 128, stdin);
+        line[strlen(line)-1] = 0;
+
+        if (line[0]==0)
+            continue;
+        pathname[0] = 0;
+
+        sscanf(line, "%s %s %s", cmd, pathname, extra_arg);
+        printf(RESET);
+        //printf("cmd=%s pathname=%s\n", cmd, pathname);
+  
+        if (strcmp(cmd, "ls")==0)
+            ls();
+        else if (strcmp(cmd, "cd")==0)
+            cd();
+        else if (strcmp(cmd, "pwd")==0)
+            pwd(running->cwd);
+        else if (strcmp(cmd, "mkdir")==0)
+            my_mkdir(pathname);
+        else if (strcmp(cmd, "rmdir")==0)
+            my_rmdir();
+        else if (strcmp(cmd, "link")==0)
+            my_link();
+        else if (strcmp(cmd, "unlink")==0)
+            my_unlink();
+        else if (strcmp(cmd, "symlink")==0)
+            my_symlink();
+        else if (strcmp(cmd, "creat")==0)
+            my_creat(pathname);
+        else if (strcmp(cmd, "quit")==0)
+            quit();
+        else if (strcmp(cmd, "clear")==0)
+            clear();
+        else if (strcmp(cmd, "cat")==0)
+            cat(pathname);
+        else if (strcmp(cmd, "cp")==0)
+            cp(pathname, extra_arg);
+        else if (strcmp(cmd, "pfd")==0)
+            print_fd();
+        else if (strcmp(cmd, "help")==0)
+            help();
+        printf("\n");
+        bzero(pathname, 128);
+    }
 }
 
 void my_banner() {
